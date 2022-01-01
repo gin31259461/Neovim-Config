4631326d04ee2ed08891e9a049969bc11e1cc3ab
abner <qw0207060413@gmail.com>
2022-01-01 21:23:47 +0800

    fix colorscheme gui color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -13,7 +13,7 @@ try
 
   " hi Normal     ctermbg=NONE guibg=NONE
   " hi SignColumn ctermbg=NONE guibg=NONE
-  hi LineNr     ctermbg=NONE guibg=NONE
+  " hi LineNr     ctermbg=NONE guibg=NONE
   hi Comment guifg=#5C6370 ctermfg=59
 
 endtry
