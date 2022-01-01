c0bed86390cfe2fba4e71a8fb38990973c1be36a
abner <qw0207060413@gmail.com>
2022-01-01 21:21:19 +0800

    fix colorscheme gui color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -11,9 +11,9 @@ try
   " set term gui colors (most terminals support this)
   set termguicolors 
 
-  hi Normal     ctermbg=NONE guibg=NONE
+  " hi Normal     ctermbg=NONE guibg=NONE
+  " hi SignColumn ctermbg=NONE guibg=NONE
   hi LineNr     ctermbg=NONE guibg=NONE
-  hi SignColumn ctermbg=NONE guibg=NONE
   hi Comment guifg=#5C6370 ctermfg=59
 
 endtry
