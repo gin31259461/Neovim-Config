63d61e1bd6082f584b394d271fb06141a8f90fec
abner <qw0207060413@gmail.com>
2022-12-16 00:07:48 +0800

    fix syntax error

--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -1,6 +1,16 @@
 local M = {}
 
-if (require('utils').WINDOWS()) then
+function OSX()
+  return vim.fn.has("macunix")
+end
+function LINUX()
+  return vim.fn.has("unix") and not vim.fn.has("macunix") and not vim.fn.has("win32unix")
+end
+function WINDOWS()
+  return vim.fn.has("win16") or vim.fn.has("win32") or vim.fn.has("win64")
+end
+
+if (WINDOWS()) then
   M.cmd = { "typescript-language-server.cmd", "--stdio" }
 else
   M.cmd = { "typescript-language-server", "--stdio" }
